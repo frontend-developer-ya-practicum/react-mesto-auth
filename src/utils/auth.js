f436e13ff420e59a16fcb42b487cb4df328db354
Александр Морев <a.morev@kryptonite.ru>
2022-10-03 08:52:13 +0300

    Add auth api

---
+++ src/utils/auth.js
@@ -0,0 +1,56 @@
+import { AUTH_ROOT } from "./environment.js";
+
+class Auth {
+  constructor({ baseUrl, headers }) {
+    this._baseUrl = baseUrl;
+    this._headers = headers;
+  }
+
+  register({ email, password }) {
+    console.log(AUTH_ROOT);
+    return fetch(this._baseUrl + "/signup", {
+      method: "POST",
+      headers: this._headers,
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+    }).then((resp) => this._checkResp(resp, "Ошибка при регистрации"));
+  }
+
+  authorize({ email, password }) {
+    return fetch(this._baseUrl + "/signin", {
+      method: "POST",
+      headers: this._headers,
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+    }).then((resp) => this._checkResp(resp, "Ошибка при авторизации"));
+  }
+
+  checkToken({ token }) {
+    const headers = { ...this._headers, Authorization: `Bearer ${token}` };
+
+    return fetch(this._baseUrl + "/users/me", {
+      method: "GET",
+      headers: headers,
+    }).then((resp) => this._checkResp(resp, "Ошибка при валидации токена"));
+  }
+
+  _checkResp(resp, errorMessage) {
+    if (resp.ok) {
+      return resp.json();
+    }
+    return Promise.reject(`${errorMessage}: ${resp.status}`);
+  }
+}
+
+const auth = new Auth({
+  baseUrl: AUTH_ROOT,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export default auth;
--- src/utils/environment.js
+++ src/utils/environment.js
@@ -1,4 +1,6 @@
 const API_ROOT = process.env.REACT_APP_API_ROOT;
 const API_TOKEN = process.env.REACT_APP_API_TOKEN;
 
-export { API_ROOT, API_TOKEN };
+const AUTH_ROOT = process.env.REACT_APP_AUTH_ROOT;
+
+export { API_ROOT, API_TOKEN, AUTH_ROOT };
